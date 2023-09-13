cbffb9b3e70802a751bb42bc72400b477d9d74df
Metadata-0 <123509616+metadata0@users.noreply.github.com>
2023-09-12 23:45:16 -0400

    instructions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom/client';
 
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
+function addSteps(stepArray, targetId) {
+  let targetDiv = document.getElementById(targetId);
+  for (let i = 0; i < stepArray.length; i++) {
+    //retrieve step from array
+    let step = stepArray[i];
+    //create empty element node for step
+    let stepNode = createElement("p");
+    //create text node to hold text
+    let stepText = document.createTextNode(step);
+    //append name to empty element node
+    stepNode.appendChild(stepText)
+    targetDiv.appendChild(stepNode)
+  }
+}
+
 function App(){
 
   const description = "Peculiar type of ration emerged from the depths of hell. Its appearance veiled in obscurity, its origins shrouded in mystery, its existence rumored to date back to the dawn of time itself."
 
-  const disclaimer = "Not a meal for the faint-hearted. The creator of this recipe shall not be held liable for any harm or injuries, whether physical or emotional, incurred by those who attempt to prepare it."
-  const step1 = ''
-  const step2 = ''
-  const step3 = ''
-  const steps = [step1, step2, step3]
+  const disclaimer = "Not a meal for the faint-hearted. The creator of this recipe shall not be held liable for any harm or injuries, whether physical or emotional, incurred by those who attempt to try it."
+  const step1 = 'Submerge instant noodle in hot water for 3 minutes (optional)'
+  const step2 = 'Add a tomato, or some orange pigments into the mix'
+  const step3 = 'Stir thoroughly'
+  const step4 = 'Throw in a piece of dark chocolate'
+  const step5 = '???'
+  const step6 = 'success!'
+  const steps = [step1, step2, step3, step4, step5, step6]
 
-
+  var stepNum = steps.length
+  // var renderedSteps = arr.map(item => <div> {item} <div/>)
 
   let deathCount = 0;
   function deathCountAdd() {
@@ -24,9 +43,14 @@ function App(){
 
   return (
     <>
+    
       <div>
         <h2>My Receipe</h2>
         <h1>Yummy Chocolate Noodle *Cursed</h1>
+      </div>
+
+      <div>
+        <img src="https://im.indiatimes.in/content/2020/Jun/Chocolate-maggi_5ef07e08bf5ab.jpg?w=1200&h=900&cc=1" style={{width:500}}></img>
       </div>
 
       <body>
@@ -36,30 +60,40 @@ function App(){
       <div id="preface">
         <div>Disclaimer:</div>
         <div>{disclaimer}</div>
-        
       </div>
 
-      <div id="steps">
-        <div>
-          step 1: {steps[0]}
-        </div>
+      <div id="stepID"> 
+        <h2>How to make it</h2>
+        {/* {addSteps(steps, stepID)} */}
+      </div>
 
-        <div>
-          step 2: {steps[0]}
-        </div>
+        
 
+      <div>
+        Step 1: {step1}
+      </div>
+      <div>
+        Step 2: {step2}
+      </div>
+      <div>
+        Step 3: {step3}
+      </div>
+      <div>
+        Step 4: {step4}
+      </div>
+      <div>
+        Step 5: {step5}
+      </div>
+      <div>
+        Step 6: {step6}
       </div>
 
       <div>
         the current puke count is {deathCount}
         <button onclick={deathCountAdd}>I puked</button>
       </div>
-
-
     </>
   )
-
-
 }
 
 
